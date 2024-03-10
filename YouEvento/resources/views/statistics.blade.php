2baea0b6bad1f2e5659c35d8fb33017ad15c8f6e
elkhailihamza <elkhailihamza331@gmail.com>
2024-03-10 19:43:22 +0000

    completed statistics page

--- YouEvento/resources/views/statistics.blade.php
+++ YouEvento/resources/views/statistics.blade.php
@@ -7,9 +7,9 @@
         <h3 class="text-3xl">Statistics:</h3>
     </div>
 </section>
-<section class="flex justify-evenly mt-10">
+<section class="flex flex-wrap justify-evenly gap-5 mt-10">
     <div class="w-[350px] rounded-md border p-2">
-        <div class="flex h-[300px] justify-center mb-3">
+        <div class="flex h-[300px] justify-center">
             <img src="{{ asset('storage/'.$event->cover) }}">
         </div>
         <div>
@@ -18,23 +18,44 @@
             <p class="truncate">Reserved on: {{$event->created_at->diffForHumans()}}</p>
         </div>
     </div>
-    <div class="mt-10">
-        @foreach ($event->tickets as $ticket)
-        <span class="block">
-            <span class="font-bold">Ticket Type:</span>
-            {{$ticket->ticket_name.': '.$ticket->ticket_price.' DH'}}
-        </span>
+    <div>
+        <h1 class="font-bold text-[26px]">Tickets:</h1>
+        <hr>
+        @php
+        $total = 0;
+        @endphp
+        <div class="flex flex-wrap gap-10 mt-5">
+            @foreach ($event->tickets as $ticket)
+            <div class="mb-2">
+                <span class="font-medium text-[26px]">{{$ticket->ticket_name}}:</span>
+                <hr>
+                <span class="block">
+                    <span class="font-medium">Ticket Price:</span>
+                    {{$ticket->ticket_price.' DH'}}
+                </span>
 
-        <span class="block">
-            <span class="font-bold">Total Reservations:</span>
-            {{$ticket->ticket_name.': '.$ticket->reservations->count()}}
-        </span>
+                <span class="block">
+                    <span class="font-medium">Total Reservations:</span>
+                    @php
+                    $ticketReservations = $ticket->reservations->count();
+                    $total += $ticketReservations;
+                    @endphp
+                    {{$ticketReservations}}
 
-        <span class="block">
-            <span class="font-bold">S:</span>
-            {{$ticket->ticket_name.': '.$ticket->reservations->count()}}
-        </span>
-        @endforeach
+                </span>
+            </div>
+            @endforeach
+        </div>
+        <h1 class="font-bold text-[26px] mt-5">Total:</h1>
+        <hr>
+        <div>
+            <span class="font-medium">Reservations:</span>
+            {{$total}}
+        </div>
+        <div>
+            <span class="font-medium">Tickets:</span>
+            {{$event->tickets->count()}}
+        </div>
     </div>
 </section>
 
